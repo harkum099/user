dc6f8d2310f6ea351eb35db52cc841ee5ceb7393
Harendra Kumar <harkum099@gmail.com>
2024-07-01 09:32:34 +0530

    added docs

--- user/src/test/java/CucumberRunnerTest.java
+++ user/src/test/java/CucumberRunnerTest.java
@@ -2,6 +2,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+/**
+ * @author harkumar13 
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"com.banking.stepdef", "com.banking.config"}
